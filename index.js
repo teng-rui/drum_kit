188731fbd6f84775085239296057246dd6b841f7
Rui Teng <tengrui1003@gmail.com>
2022-11-23 09:53:51 +0200

    modify sound

--- index.js
+++ index.js
@@ -1,50 +1,80 @@
-// function $(x) {
-//     return document.getElementById(x);
-// }
-// let drum_1=$('.w');
-// console.log(drum_1);
-// drum_1.on("click", function(){handleClick(0);});
+
 
 
 let drum = document.getElementsByClassName("drum");
-let sounds=[];
-sounds.push(new Audio("sounds/crash.mp3"));
-sounds.push(new Audio("sounds/kick-bass.mp3"));
-sounds.push(new Audio("sounds/snare.mp3"));
-sounds.push(new Audio("sounds/tom-1.mp3"));
-sounds.push(new Audio("sounds/tom-2.mp3"));
-sounds.push(new Audio("sounds/tom-3.mp3"));
-sounds.push(new Audio("sounds/tom-4.mp3"));
 
+function buttonAnimation(order) {
 
+    var activeButton = drum[order];
+    activeButton.classList.add("pressed");
 
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
 
-function handleClick(order) {
-
-    sounds[order].play();
-    drum[order].classList.add('pressed');
-    setTimeout(function(){
-        drum[order].classList.remove('pressed',100);
-    })
 }
 
-function handleKeypress(key) {
-    switch(key){
-        case 'w':handleClick(0);
-        case 'a':handleClick(1);
-        case 's':handleClick(2);
-        case 'd':handleClick(3);
-        case 'j':handleClick(4);
-        case 'k':handleClick(5);
-        case 'l':handleClick(6);
+
+
+
+
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            let sound_crash = new Audio('./sounds/crash.mp3');
+            sound_crash.play();
+            buttonAnimation(0);
+            break;
+        case 'a':
+            let sound_kick = new Audio('./sounds/kick-bass.mp3');
+            sound_kick.play();
+            buttonAnimation(1);
+            break;
+        case 's':
+            let sound_snare = new Audio('./sounds/snare.mp3');
+            sound_snare.play();
+            buttonAnimation(2);
+            break;
+        case 'd':
+            let sound_tom1 = new Audio('./sounds/tom-1.mp3');
+            sound_tom1.play();
+            buttonAnimation(3);
+            break;
+
+        case 'j':
+            let sound_tom2 = new Audio('./sounds/tom-2.mp3');
+            sound_tom2.play();
+            buttonAnimation(4);
+            break;
+
+        case 'k':
+            let sound_tom3 = new Audio('./sounds/tom-3.mp3');
+            sound_tom3.play();
+            buttonAnimation(5);
+            break;
+
+        case 'l':
+            let sound_tom4 = new Audio('./sounds/tom-4.mp3');
+            sound_tom4.play();
+            buttonAnimation(6);
+            break;
     }
+
+
 }
 
-for (let i=0;i<7;i++){
-    drum[i].addEventListener("click", function(){handleClick(i);});
-}
 
-document.addEventListener("keypress",function (event){
-    handleKeypress(event.key);
-
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
 })
+
+for (let i = 0; i < 7; i++) {
+    drum[i].addEventListener("click", function () {
+        makeSound(drum[i].innerHTML);
+
+    });
+}
+
+
+
+
